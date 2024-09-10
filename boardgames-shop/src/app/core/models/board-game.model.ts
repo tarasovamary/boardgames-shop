3cf2210d4886f4bfb207db1bba7c545f15b96b17
Mariya Kryvitskaya <masha.oath@gmail.com>
2024-09-10 11:05:59 +0300

    feat: add boardgame interface

---
+++ boardgames-shop/src/app/core/models/board-game.model.ts
@@ -0,0 +1,17 @@
+export interface BoardGame {
+  id: number;
+  name: string;
+  price: number;
+  images: string[];
+  title: string;
+  description: string;
+  yearpublished: number;
+  rating: string;
+  designer: string;
+  minplayers: number;
+  maxplayers: number;
+  playingtime: number;
+  minage: number;
+  type: string[];
+  category: string[];
+}
