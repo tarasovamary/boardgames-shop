ad4dbe73822ab19ef22b7ada5442b9bcb2bd663c
Mariya Kryvitskaya <masha.oath@gmail.com>
2024-09-11 16:13:50 +0300

    feat: add cart item count

--- boardgames-shop/src/app/shared/components/header/header.component.ts
+++ boardgames-shop/src/app/shared/components/header/header.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { Game } from '../../../core/models/game.model';
+import { selectCartItems } from '../../../core/store/cart/cart.selector';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
+  gamesInCart$: Observable<Game[]> = this.store.select(selectCartItems);
 
+  constructor(private store: Store) {}
 }
