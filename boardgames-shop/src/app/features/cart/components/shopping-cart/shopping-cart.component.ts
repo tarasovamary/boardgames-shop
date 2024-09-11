272f63668ecf2186bd547b41d46499eb01a6856e
Mariya Kryvitskaya <masha.oath@gmail.com>
2024-09-11 15:08:15 +0300

    feat: add subtotal price in a cart

--- boardgames-shop/src/app/features/cart/components/shopping-cart/shopping-cart.component.ts
+++ boardgames-shop/src/app/features/cart/components/shopping-cart/shopping-cart.component.ts
@@ -1,17 +1,17 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 import { Game } from '../../../../core/models/game.model';
-import { Store } from '@ngrx/store';
-import { selectCartItems } from '../../../../core/store/cart/cart.selector';
+import { selectCartItems, selectCartTotalPrice } from '../../../../core/store/cart/cart.selector';
 
 @Component({
   selector: 'app-shopping-cart',
   templateUrl: './shopping-cart.component.html',
-  styleUrl: './shopping-cart.component.scss'
+  styleUrl: './shopping-cart.component.scss',
 })
 export class ShoppingCartComponent {
   games$: Observable<Game[]> = this.store.select(selectCartItems);
+  totalPrice$: Observable<number> = this.store.select(selectCartTotalPrice);
 
-  constructor(private store: Store){}
-
+  constructor(private store: Store) {}
 }
